c8b5eb2388905875c590a5d0d08d097289ab1b9c
GustavoMolaison <hubertwolowiec1102@gmail.com>
2024-05-15 21:50:02 +0200

    alpha0.5
    login complete

--- code/Learn_it_api.py
+++ code/Learn_it_api.py
@@ -1,5 +1,14 @@
 from tkinter import *
-screen = Tk()
-screen.title("Learn_It!")
-screen.mainloop()
-screen.geometry("500x500")
+def Learn_it_main():
+    Ltm_screen = Tk()
+    # Ltm_screen.configure(bg='black')
+    # Ltm_screen.attributes('-fullscreen',True)
+    Ltm_screen.title('LearnIt!')
+    Ltm_screen.geometry('1920x1080')
+    lil = ('Kozuka Mincho Pro M', 50, 'bold')
+    Learn_it_label = Label(text='LearnIt!', bg='black', fg='yellow', height=2, width=10)
+    Learn_it_label.configure(font=lil)
+    Learn_it_label.place(y=0, x=725)
+    Ltm_screen.mainloop()
+
+Learn_it_main()
--- code/visualization_log.py
+++ code/visualization_log.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import pickle
+from Learn_it_api import Learn_it_main
 
 
 def login_error(le_variable):
@@ -27,7 +28,11 @@ def login():
             elif not username_and_password[username_text] == password_text:
                 login_error('incorrect password')
             else:
-                print('you loged in')
+                screen.destroy()
+                Learn_it_main()
+                # funkcja xd
+
+
 
 
 def gb():
@@ -49,66 +54,84 @@ def registration():
     password_text = str(password.get())
     if not len(username_text) >= 3:
         reg_error_input = Label(text='username must contain at least 3 letters', fg='white', bg='black', height=2,
-                          width=len('username must contain at least 3 letters'))
+                                width=len('username must contain at least 3 letters'))
         reg_error_input.place(x=400, y=120)
     elif username_text in username_and_password.keys():
-        reg_error_input.destroy()
-        reg_error_input = Label(text='Sadly this username is occupied by another user', fg='white', bg='black', height=2,
-                               width=len('Sadly username is occupied by another user'))
+        try:
+            reg_error_input.destroy()
+        except:
+            pass
+        reg_error_input = Label(text='Sadly this username is occupied by another user', fg='white', bg='black',
+                                height=2,
+                                width=len('Sadly username is occupied by another user'))
         reg_error_input.place(x=400, y=120)
     else:
         if password_text in username_and_password.values():
-            reg_error_input.destroy()
+            try:
+                reg_error_input.destroy()
+            except:
+                pass
             reg_error_input = Label(text='Sadly this password is occupied by another user', fg='white', bg='black',
-                                   height=2,
-                                   width=len('Sadly this password is occupied by another user'))
+                                    height=2,
+                                    width=len('Sadly this password is occupied by another user'))
             reg_error_input.place(x=400, y=120)
         elif len(password_text) < 6:
-            reg_error_input.destroy()
+            try:
+                reg_error_input.destroy()
+            except:
+                pass
             reg_error_input = Label(text='password must contain at least 6 letters', fg='white', bg='black', height=2,
-                              width=len('password must contain at least 6 letters'))
+                                    width=len('password must contain at least 6 letters'))
             reg_error_input.place(x=400, y=120)
         else:
             for big_letter in password_text:
                 if big_letter.isupper():
                     big_letter_count += 1
             if not big_letter_count > 1:
-                reg_error_input.destroy()
+                try:
+                    reg_error_input.destroy()
+                except:
+                    pass
                 reg_error_input = Label(text='password must contain at least 1  big letter', fg='white', bg='black',
-                                   height=2,
-                                   width=len('password must contain at least 1  big letter'))
+                                        height=2,
+                                        width=len('password must contain at least 1  big letter'))
                 reg_error_input.place(x=400, y=120)
             if big_letter_count >= 1:
                 for pan in password_text:
                     special_character = ['!', '@', "#", "$", '%', '^', '&', "*", "(", ')', '-', '_', '=', '+', '/', "?",
                                          '>', '<', "`"]
                     if pan in special_character:
-                      m = True
+                        m = True
                 if m:
-                    # username_and_password[username_text] = password_text
-                    # with open('usernames_and_passwords', 'wb') as file:
-                    #     pickle.dump(username_and_password, file)
+                    username_and_password[username_text] = password_text
+                    with open('usernames_and_passwords', 'wb') as file:
+                        pickle.dump(username_and_password, file)
                     goback_button = Button(screen, text='go back', bg='black', fg='yellow', width=15, command=gb)
                     reg_com_input = Label(text='Registration complete!', fg='white', bg='black',
-                                       height=2, width=len('Registration complete!'))
+                                          height=2, width=len('Registration complete!'))
                     reg_com_input.place(x=465, y=400)
                     goback_button.place(x=480, y=360)
                     try:
-                     reg_error_input.destroy()
-                    except NameError:
+                        reg_error_input.destroy()
+                    except:
                         pass
                     user_entry.delete(0, END)
                     pass_entry.delete(0, END)
 
                 else:
-                    reg_error_input.destroy()
-                    reg_error_input = Label(text='password must contain at least 1 special character', fg='white', bg='black',
-                                      height=2, width=len('password must contain at least 1 special character'))
+                    try:
+                        reg_error_input.destroy()
+                    except:
+                        pass
+                    reg_error_input = Label(text='password must contain at least 1 special character', fg='white',
+                                            bg='black',
+                                            height=2, width=len('password must contain at least 1 special character'))
                     reg_error_input.place(x=400, y=120)
 
 
 def register():
     login_button.destroy()
+    reg_button0.destroy()
     reg_button = Button(screen, text='registration', width=20, bg='black', fg='yellow', command=registration)
     reg_button.place(x=465, y=320)
     goback_button = Button(screen, text='go back', bg='black', fg='yellow', width=15, command=gb)
@@ -118,7 +141,7 @@ def register():
 
 
 def login_screen():
-    global screen, password, username, login_button, reg_button, pass_entry, user_entry
+    global screen, password, username, login_button, reg_button0, pass_entry, user_entry
     screen = Tk()
     screen.geometry('1000x500')
     screen.title('Python form validation')
@@ -136,9 +159,9 @@ def login_screen():
     pass_entry.place(x=485, y=280)
 
     login_button = Button(screen, text='login', width=20, bg='black', fg='yellow', command=login)
-    reg_button = Button(screen, text='registration', width=20, bg='black', fg='yellow', command=register)
+    reg_button0 = Button(screen, text='registration', width=20, bg='black', fg='yellow', command=register)
     login_button.place(x=465, y=320)
-    reg_button.place(x=465, y=360)
+    reg_button0.place(x=465, y=360)
     screen.mainloop()
 
 
